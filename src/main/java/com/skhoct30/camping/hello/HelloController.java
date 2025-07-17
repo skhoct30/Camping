095c2fc87b621a36e387c3f2fb5ee6b497e186b7
skhoct30 <skhoct30@naver.com>
2025-07-17 18:23:26 +0900

    [서강현] - 캠핑장 예약 홈페이지 프로젝트 - 프로젝트 기본 설정 - thymeleaf, mysql, mybatis 설정

---
+++ src/main/java/com/skhoct30/camping/hello/HelloController.java
@@ -0,0 +1,23 @@
+package com.skhoct30.camping.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "Hello World";
+	}
+	
+	
+	
+	@GetMapping("/hello/thymeleaf")
+	public String helloThymeleaf() {
+		return "hello/hello";
+	}
+	
+}
---
+++ src/test/java/com/skhoct30/camping/CampingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skhoct30.camping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
